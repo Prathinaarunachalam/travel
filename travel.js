c4be98855d95957d46aece81f97f1200ac0d9122
prathina <prathinaarunachalam@gmail.com>
2023-10-29 21:49:11 +0530

    travel2

--- travel.js
+++ travel.js
@@ -1,59 +1,19 @@
+function fname(){
+    window.alert("The first name should be in uppercase");
+}
+
+function lname(){
+    window.alert("The last name should be in uppercase");
+}
+
+function email(){
+    window.alert("The email should have atleast one '@' and the email should be in use");
+}
+
+
 function mytrav(){
     window.alert("We have got your message successfully");
 }
 
-function onmover(){
-    window.alert("onmouseover");
-}
 
-function onmdown(){
-    window.alert("onmousedown");
-}
-
-function onmleave(){
-    window.alert("onmouseleave");
-}
-
-function ondbcl(){
-    window.alert("ondblclick");
-}
-
-const objs=()=>{
-const obj = {
-    name : "abc",
-    rollno : 33,
-    o1:teddy={
-        name:"teddy",
-        rollno:13,
-    },
-    o2:zara={
-        name:"zara",
-        rollno:32,
-    }
-};
-window.alert(obj.o2.name);
-}
-
-function constructor(a,b){
-    this.a=a;
-    this.b=b;  
-}
-
-const o5 = new constructor(10,30);
-let c = o5.a+o5.b
-function trial(){
-    window.alert(c);
-}
-
-function changecolor(){
-    let input =document.getElementById('fname');
-    input.style.backgroundColor='blue';
-}
-
-function changecolor1(){
-    document.getElementById("trial").style.backgroundColor='black';
-}
-
-function changecolor2(){
-    document.getElementById("trial").style.backgroundColor='white';
-}
+window.alert("Do you want us to keep you updated");
